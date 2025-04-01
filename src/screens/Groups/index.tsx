44f050b77783e06f765b492134efd41f65d9e814
felipeWanderson <felipewanderson2020@gmail.com>
2025-04-01 18:36:51 -0300

    feat: list of teams

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from './styles'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
+import { FlatList } from 'react-native'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Turma da Bocha", "Familia"]);
+
   return (
     <Container>
       <Header />
       <Highlight title='Turmas' subtitle='jogue com a sua turma'/>
 
-      <GroupCard title='Turma da bocha'/>
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item}
+          />
+        )}
+      />
     </Container>
   )
 }
