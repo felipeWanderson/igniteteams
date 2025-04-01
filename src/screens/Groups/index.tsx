cc9cd4f912c761169cd88b032208f8a0bce5bbc2
felipeWanderson <felipewanderson2020@gmail.com>
2025-04-01 18:12:40 -0300

    feat: component groupcard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Container } from './styles'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
+import { GroupCard } from '@components/GroupCard'
 
 export function Groups() {
   return (
     <Container>
       <Header />
       <Highlight title='Turmas' subtitle='jogue com a sua turma'/>
+
+      <GroupCard title='Turma da bocha'/>
     </Container>
   )
 }
