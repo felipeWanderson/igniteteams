427d944754108a7761a2c3ceb48f67664505bac7
felipeWanderson <felipewanderson2020@gmail.com>
2025-04-03 17:36:53 -0300

    feat: player card

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,10 +6,12 @@ import { ButtonIcon } from '@components/ButtonIcon'
 import { Input } from '@components/Input'
 import Filter from '@components/Filter'
 import { FlatList } from 'react-native'
+import { PlayerCard } from '@components/PlayerCard'
+import { Button } from '@components/Button'
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPalyers] = useState([]);
+  const [players, setPalyers] = useState(['Felipe', 'Vini']);
 
   return (
     <Container>
@@ -48,6 +50,24 @@ export function Players() {
           {players.length}
         </NumberOfPlayers>
        </HeaderList>
+
+       <FlatList 
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard 
+            name={item}
+            onRemove={() => {}}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[{paddingBottom: 100}, players.length === 0 && { flex: 1}]}
+       />
+
+       <Button 
+        title='Remover Turma'
+        type='SECONDARY'
+       />
     </Container>
   ) 
 }
